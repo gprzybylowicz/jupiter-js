84435be8b426bac6ab899f9ffbf38ccd866d5dfd
Grzegorz Przybyłowicz <grzegorz.p@grzegorzp-MacBook-Pro.local>
2015-05-07 22:58:58 +0200

    write/read config tests for position and size behaviours

--- src/config/SizeConfigParser.js
+++ src/config/SizeConfigParser.js
@@ -8,9 +8,19 @@ SizeConfigParser.prototype = Object.create(Parser.prototype);
 SizeConfigParser.prototype.constructor = SizeConfigParser;
 
 SizeConfigParser.prototype.write = function(config) {
+	config.allowNegativeValues = this._behaviour.allowNegativeValues;
+	config.sizeStart = this._writePoint(this._behaviour.sizeStart);
+	config.sizeEnd = this._writePoint(this._behaviour.sizeEnd);
+	config.startVariance = this._behaviour.startVariance;
+	config.endVariance = this._behaviour.endVariance;
 };
 
 SizeConfigParser.prototype.read = function(config) {
+	this._behaviour.allowNegativeValues = config.allowNegativeValues;
+	this._behaviour.sizeStart = this._readPoint(config.sizeStart);
+	this._behaviour.sizeEnd = this._readPoint(config.sizeEnd);
+	this._behaviour.startVariance = config.startVariance;
+	this._behaviour.endVariance = config.endVariance;
 };
 
 module.exports = SizeConfigParser;
---
+++ test/src/config/PositionBehaviourParserTest.js
@@ -0,0 +1,47 @@
+var PositionBehaviour = require("jupiter").PositionBehaviour;
+var PositionConfigParser = require("jupiter").config.PositionConfigParser;
+var Point = require("jupiter").Point;
+
+describe("PositionBehaviourParserTest", function() {
+	it("write", function() {
+		var behaviour = new PositionBehaviour();
+		behaviour.position.set(2, 3);
+		behaviour.positionVariance.set(3, 4);
+		behaviour.velocity.set(10, 11);
+		behaviour.velocityVariance.set(20, 21);
+		behaviour.acceleration.set(30, 31);
+		behaviour.accelerationVariance.set(40, 41);
+
+		var target = {
+			position: {x: 2, y: 3},
+			positionVariance: {x: 3, y: 4},
+			velocity: {x: 10, y: 11},
+			velocityVariance: {x: 20, y: 21},
+			acceleration: {x: 30, y: 31},
+			accelerationVariance: {x: 40, y: 41}
+		};
+		var config = {};
+		new PositionConfigParser(behaviour).write(config);
+		assert.deepEqual(config, target);
+	});
+
+	it("read", function() {
+		var behaviour = new PositionBehaviour();
+
+		var config = {
+			position: {x: 2, y: 3},
+			positionVariance: {x: 3, y: 4},
+			velocity: {x: 10, y: 11},
+			velocityVariance: {x: 20, y: 21},
+			acceleration: {x: 30, y: 31},
+			accelerationVariance: {x: 40, y: 41}
+		};
+		new PositionConfigParser(behaviour).read(config);
+		assert.deepEqual(behaviour.position, new Point(2, 3));
+		assert.deepEqual(behaviour.positionVariance, new Point(3, 4));
+		assert.deepEqual(behaviour.velocity, new Point(10, 11));
+		assert.deepEqual(behaviour.velocityVariance, new Point(20, 21));
+		assert.deepEqual(behaviour.acceleration, new Point(30, 31));
+		assert.deepEqual(behaviour.accelerationVariance, new Point(40, 41));
+	});
+});
