9b1dbb1dff793336ea031331d745c72fbb6c04a3
Grzegorz Przybyłowicz <grzegorz.p@grzegorzp-MacBook-Pro.local>
2015-05-16 15:31:57 +0200

    fix life write/read config tests

--- test/src/config/BehavioursParserLifeTest.js
+++ test/src/config/BehavioursParserLifeTest.js
@@ -1,33 +1,32 @@
-//var LifeBehaviour = require("jupiter").LifeBehaviour;
-//var LifeConfigParser = require("jupiter").config.LifeConfigParser;
-//
-//describe("BehaviourParserTest - Life", function() {
-//	//it("write", function() {
-//	//	var behaviour = new LifeBehaviour();
-//	//	behaviour.maxLifeTime = 1234;
-//	//	behaviour.timeVariance = 100;
-//	//
-//	//	var target = {
-//	//		time: 1234,
-//	//		variance: 100
-//	//	};
-//	//
-//	//	var config = {};
-//	//	new LifeConfigParser(behaviour).write(config);
-//	//	assert.deepEqual(config, target);
-//	//});
-//	//
-//	//it("read", function() {
-//	//	var behaviour = new LifeBehaviour();
-//	//
-//	//	var config = {
-//	//		time: 888,
-//	//		variance: 111
-//	//	};
-//	//
-//	//	new LifeConfigParser(behaviour).read(config);
-//	//	assert.equal(behaviour.maxLifeTime, config.time);
-//	//	assert.equal(behaviour.timeVariance, config.variance);
-//	//});
-//
-//});
+var LifeBehaviour = require("jupiter").LifeBehaviour;
+var BehaviourParser = require("jupiter").config.BehaviourParser;
+
+describe("BehaviourParserLifeTest", function() {
+
+	var TARGET_CONFIG = {
+		LifeBehaviour: {
+			priority: LifeBehaviour.DEFAULT_PRIORITY,
+			maxLifeTime: 1234,
+			timeVariance: 100
+		}
+	};
+
+	it("write", function() {
+		var behaviour = new LifeBehaviour();
+		behaviour.maxLifeTime = 1234;
+		behaviour.timeVariance = 100;
+
+		var config = {};
+		behaviour.getConfigParser().write(config);
+		assert.deepEqual(config, TARGET_CONFIG);
+	});
+
+	it("read", function() {
+		var behaviour = new LifeBehaviour();
+
+		behaviour.getConfigParser().read(TARGET_CONFIG.LifeBehaviour);
+		assert.equal(behaviour.maxLifeTime, 1234);
+		assert.equal(behaviour.timeVariance, 100);
+	});
+
+});
