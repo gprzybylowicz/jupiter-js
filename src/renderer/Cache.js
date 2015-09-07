d0137ab3a9e1747ce48f731ac0ede5bb4454ff3e
Grzegorz Przybyłowicz <grzegorz.p@grzegorzp-MacBook-Pro.local>
2015-09-07 20:43:09 +0200

    added Cache.js and factory method

---
+++ src/renderer/Cache.js
@@ -0,0 +1,21 @@
+function Cache() {
+	this.data = {};
+}
+
+Cache.prototype.add = function(config) {
+	this.data[config.meta.name] = config;
+};
+
+Cache.prototype.get = function(name) {
+	if (!this.has(name)) {
+		throw new Error("\"" + name + "\" config doesn't exist in cache");
+	}
+
+	return this.data[name];
+};
+
+Cache.prototype.has = function(name) {
+	return !!this.data[name];
+};
+
+module.exports = Cache;
--- src/renderer/pixi/index.js
+++ src/renderer/pixi/index.js
@@ -5,4 +5,17 @@ var jupiter = {
 module.exports = jupiter;
 
 var engine = require("../../engine");
+var Cache = require("../Cache.js");
+
+jupiter.cache = new Cache();
 Object.assign(jupiter, engine);
+
+jupiter.create = function(configName, textureName) {
+	var config = jupiter.cache.get(configName);
+	var texture = PIXI.Texture.fromFrame(textureName);
+
+	var emitter = new engine.Emitter();
+	emitter.getParser().read(config);
+	return new jupiter.Renderer(emitter, texture);
+};
+
