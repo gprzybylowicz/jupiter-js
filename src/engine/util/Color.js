d759517647a05960633e81a3c90bfb5305fecd32
Grzegorz Przybyłowicz <grzegorz.p@grzegorzp-MacBook-Pro.local>
2015-09-16 13:31:17 +0200

    small fix in Color.js

--- src/engine/util/Color.js
+++ src/engine/util/Color.js
@@ -32,7 +32,7 @@ Color.prototype.set = function(r, g, b, a) {
 	this.r = r || 0;
 	this.g = g || 0;
 	this.b = b || 0;
-	this.alpha = a || 0;
+	this.alpha = a;
 };
 
 Object.defineProperty(Color.prototype, "hex", {
